d85305fdda2924ec0f1671fba846886a99e7d50f
Phan Thái Hiền <57438688+ThaiHien-2k@users.noreply.github.com>
2023-06-01 08:20:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ const adminRouter = require('./routes/adminRouter');
 const bloodDonateRouter = require('./routes/bloodDonateRouter');
 
 
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // requiring middlewares
 const errorMiddleware = require('./middleware/Error');
 
