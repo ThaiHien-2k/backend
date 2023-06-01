f40fa500664567acd2556f321795082246eefb1f
Phan Thái Hiền <57438688+ThaiHien-2k@users.noreply.github.com>
2023-06-01 14:37:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ connectToDb();
 // using middlewares
 app.use(
   cors({
-    origin: [/netlify\.app$/, /localhost:\d{4}$/],
+    origin: [/netlify\.app$/, /localhost:\d{4}$/,'https://descriptive-accessible-arch.glitch.me' ],
     credentials: true,
   })
 );
@@ -55,11 +55,11 @@ app.use(express.json({ limit: '20mb' }));
 app.use(cookieParser());
 
 // basic api route
- app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
+//  app.all('/', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next()
+//   });
 app.get('/', (req, res) => {
    res.header("Access-Control-Allow-Origin"," *")
   res.status(200).json({
